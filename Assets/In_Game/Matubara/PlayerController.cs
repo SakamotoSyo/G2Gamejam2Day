71117da1659931b8b553b806d3c21a1956728e13
ESO-gardo-kia <114640824+ESO-gardo-kia@users.noreply.github.com>
2023-09-26 16:15:59 +0900

    ミニマップ作り直し

--- Assets/In_Game/Matubara/PlayerController.cs
+++ Assets/In_Game/Matubara/PlayerController.cs
@@ -1,23 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-[RequireComponent(typeof(Rigidbody))]
 
-public class PlayerController : MonoBehaviour, IPlayer
+public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float _moveSpeed = 1.0f;
-    [SerializeField]
-    private bool _godMode = false;
-    public float Speed {  get => _moveSpeed;  set => _moveSpeed += value; }
-    public bool God {  get => _godMode; set => _godMode = value; }
     private Rigidbody _rb = null;
-    private float _h;
-    private float _v;
-    private float _jump;
-    private bool _isGrounded = false;
-    [SerializeField]
-    private float _jumpPower = 1.0f;
+    private float _x;
+    private float _y;
 
     private void Start()
     {
@@ -26,45 +15,11 @@ public class PlayerController : MonoBehaviour, IPlayer
 
     private void Update()
     {
-        _h = Input.GetAxisRaw("Horizontal");
-        _v = Input.GetAxisRaw("Vertical");
-        _jump = Input.GetAxisRaw("Jump");
+        
     }
 
     private void FixedUpdate()
     {
-        Move();
-        if (_jump > 0 && _isGrounded)
-        {
-            _rb.AddForce(transform.up * _jumpPower, ForceMode.Impulse);
-        }
-    }
-
-    private void Move()
-    {
-        Vector3 dir = Vector3.forward;
-        dir.y = 0;
-        dir = dir.normalized * _moveSpeed;
-        float y = _rb.velocity.y;
-
-        _rb.velocity = dir * _moveSpeed + Vector3.up * y;
-    }
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            _isGrounded = true;
-        }
-        else if (other.gameObject.TryGetComponent<IItem>(out IItem item))
-        {
-            item.Execute(this);
-        }
-    }
-    private void OnTriggerExit(Collider other)
-    {
-        if (other.gameObject.tag == "Ground")
-        {
-            _isGrounded = false;
-        }
+        
     }
 }
