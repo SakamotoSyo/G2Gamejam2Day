bc696f50e143bb381dd1b69d2bd7f2ee8b52cf7c
MatubaraTooru <kurakura.ingen@gmail.com>
2023-09-27 11:51:36 +0900

    [Update] プレイヤーの状態に応じてエフェクトが再生されるように変更

--- Assets/In_Game/Matubara/IPlayer.cs
+++ Assets/In_Game/Matubara/IPlayer.cs
@@ -3,4 +3,8 @@ public interface IPlayer
 {
     public float Speed { get; set; }
     public bool God { get; set; }
+    public void AccelerationEffect();
+    public void DecelerationEffect();
+    public void InvincibleEffectOn();
+    public void InvincibleEffectOff();
 }
